3cd5ecd3c1bbac9948a0ef04f87abf48fc58cdaf
David Farrell <davidjfarrell96+2@gmail.com>
2026-02-23 08:55:26 +1100

    Switch to use .so

--- verilate.sh
+++ verilate.sh
@@ -1,3 +1,3 @@
 verilator  src/*.v \
-	--top-module top  --x-assign unique --x-initial unique -Wno-fatal -Isrc -Iinclude -cc -CFLAGS -fpermissive -CFLAGS -Wno-error --trace-fst -exe verilator/sim_io.cpp verilator/sim_ctrl.cpp verilator/sim_main.cpp \
+	--top-module top  --x-assign unique --x-initial unique -Wno-fatal -Isrc -Iinclude -cc -CFLAGS "-fpermissive -Wno-error"  -LDFLAGS "-lM" --trace-fst -exe verilator/sim_main.cpp verilator/sim_io.cpp \
 	&& make -C obj_dir -j -f Vtop.mk Vtop
--- verilator/sim_main.h
+++ verilator/sim_main.h
@@ -9,14 +9,19 @@
 #include <cstdint>
 #include <cstring>
 #include <iostream>
+
+#include <libM/m_lib.h>
+
 #include "sim_main.h"
-#include "sim_ctrl.h"
+//#include "sim_ctrl.h"
 #include "sim_io.h"
 #include "math.h"
 
+#ifndef VERILATOR
+#define VERILATOR
+#endif
 
-#define MAX_SAMPLES		768
-//#define PRINT_STATE
+#define MAX_SAMPLES		2048
 //#define RUN_EMULATOR
 
 #define DUMP_WAVEFORM
